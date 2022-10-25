6ee05db37d759128e037c0bd484270ab8400999d
Jason Hitching <jason_hitching@tjxeurope.com>
2022-10-25 12:11:19 +0100

    refactored to module for data import / seed, prevented timestamps

---
+++ booking-data.js
@@ -0,0 +1,14 @@
+let bookingData = [
+  {
+    title: "Physio for my bad back",
+    content: "Wish I had a better chair",
+  },
+  {
+    title: "Haircut",
+    content: "Looking rough",
+  },
+];
+
+export const { title, content } = bookingData;
+
+export default bookingData;
--- booking-model.js
+++ booking-model.js
@@ -1,19 +1,52 @@
-const Sequelize = require("sequelize");
+import bookingData from "./booking-data.js";
+import { Sequelize, DataTypes } from "sequelize";
 
-const sequelize = new Sequelize("hello_world_db", "root", "root1234", {
+/**
+ * Instantiate sequelize object
+ * with required args
+ */
+const sequelize = new Sequelize("bookings_db", "root", "root1234", {
   host: "127.0.0.1",
   dialect: "mysql",
-  dialectModule: require("mysql2"),
 });
 
 /**
- * Method for instantiating database connection to app
+ * Define our 'Model' (table) & columns
+ * Prevent auto timestamping
+ */
+const Booking = sequelize.define(
+  "Booking",
+  {
+    title: DataTypes.STRING(150),
+    content: DataTypes.TEXT("medium"),
+  },
+  {
+    timestamps: false,
+  }
+);
+
+/**
+ * Method for instantiating database connection to app,
+ * authenticating success / failure and logging SQL commands
  */
 sequelize
-  .authenticate()
+  .sync({ logging: console.log, force: true, timestamps: false })
+  .then(() => {
+    seedData();
+  })
   .then(() => {
     console.log("Connection has been established!");
   })
   .catch((err) => {
     console.log("Uhh ohhh, unable to connect to db ", err);
   });
+
+function seedData() {
+  for (let booking of bookingData) {
+    console.log(booking.title);
+    Booking.create({
+      title: booking.title,
+      content: booking.content,
+    });
+  }
+}
