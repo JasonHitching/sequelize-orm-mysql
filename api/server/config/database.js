82d583360c0741c8c0142692b1af7aed95c94779
Jason Hitching <jason_hitching@tjxeurope.com>
2022-10-26 11:30:01 +0100

    added placeholder variables for people to input their own user and pass

--- api/server/config/database.js
+++ api/server/config/database.js
@@ -1,10 +1,18 @@
 import Sequelize from "sequelize";
 
 /**
+ * Local database instance of MySQL needs to be running
+ * {yourUsername} likely === root
+ * {yourPassword} - whatever password you set within MySQL
+ */
+const yourUsername = "<username-here>";
+const yourPassword = "<password-here>";
+
+/**
  * Instantiate sequelize object
  * with required args
  */
-const sequelize = new Sequelize("bookings_db", "root", "root1234", {
+const sequelize = new Sequelize("bookings_db", yourUsername, yourPassword, {
   host: "127.0.0.1",
   dialect: "mysql",
 });
